878a6a64e4361c452ecd19d1c5547e2a190b8398
Marta Alcalde-Herraiz <91142894+martaalcalde@users.noreply.github.com>
2025-06-06 11:32:17 +0100

    Update mockMeasurementDiagnostics.R

--- R/mockMeasurementDiagnostics.R
+++ R/mockMeasurementDiagnostics.R
@@ -42,7 +42,7 @@ mockMeasurementDiagnostics <- function(nPerson = 100,
     omock::mockObservation(seed = seed) |>
     omock::mockMeasurement(seed = seed) |>
     omock::mockProcedureOccurrence(seed = seed) |>
-    omock::mockCohort(name = "my_cohort", numberCohorts = 2)
+    omock::mockCohort(name = "my_cohort", numberCohorts = 2, seed = seed)
   cdm_local$measurement <- cdm_local$measurement |>
     dplyr::mutate(
       unit_concept_id = dplyr::if_else(dplyr::row_number()%%2 == 0, 9529, NA),
